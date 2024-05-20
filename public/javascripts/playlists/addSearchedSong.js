900553e53a209a52568f0c1a3452b3418150a8e7
Axel <axtrvb@gmail.com>
2024-05-20 18:07:30 +0200

    Cleanup some code

--- public/javascripts/playlists/addSearchedSong.js
+++ public/javascripts/playlists/addSearchedSong.js
@@ -4,7 +4,7 @@ data['numero'] = 1;
 
 $('#songSearch').on('click', function () {
     var search = $('#songSearchInput').val();
-    var element = document.querySelector('#musicChooser');
+    const element = document.querySelector('#musicChooser');
     $.ajax({
         url: '/api/get-song',
         type: 'GET',
@@ -89,16 +89,16 @@ $('#songSearch').on('click', function () {
 
 
 async function addSong(element) {
-    var destination = document.querySelector('#listaCanzoni');
+    const destination = document.querySelector('#listaCanzoni');
 
-    var id = element.querySelector('input[name="songId[]"]').value;
-    var titolo = element.querySelector('input[name="songName[]"]').value;
-    var autore = element.querySelector('input[name="songAuthor[]"]').value;
-    var logo = element.querySelector('input[name="songImage[]"]').value;
-    var durata = element.querySelector('input[name="songDuration[]"]').value;
-    var releaseDate = element.querySelector('input[name="songReleaseDate[]"]').value;
-    var genre = element.querySelector('input[name="songGenre[]"]').value;
-    var songPreview = element.querySelector('input[name="songPreview[]"]').value;
+    const id = element.querySelector('input[name="songId[]"]').value;
+    const titolo = element.querySelector('input[name="songName[]"]').value;
+    const autore = element.querySelector('input[name="songAuthor[]"]').value;
+    const logo = element.querySelector('input[name="songImage[]"]').value;
+    const durata = element.querySelector('input[name="songDuration[]"]').value;
+    const releaseDate = element.querySelector('input[name="songReleaseDate[]"]').value;
+    const genre = element.querySelector('input[name="songGenre[]"]').value;
+    const songPreview = element.querySelector('input[name="songPreview[]"]').value;
     const year = releaseDate.substring(0, 4);
 
 
@@ -116,7 +116,7 @@ async function addSong(element) {
     if (data['numero'] === 1)
         destination.innerHTML = '';
 
-             var song = `<div class="list-group-item">
+    const song = `<div class="list-group-item">
     <div class="row g-2 align-items-center">
         <input type="hidden" name="songId[]" value="${id}">
         <input type="hidden" name="songName[]" value="${titolo}">
@@ -169,95 +169,6 @@ $('#modal-scrollable').on('hidden.bs.modal', function () {
     $('#modal-report').modal('show');
 });
 
-// $('#songSearch').on('click', function () {
-//
-//     var search = $('#songSearchInput').val();
-//     var element = document.querySelector('#listaCanzoni');
-//
-//     if (search.length <= 0)
-//         return;
-//
-//
-//     $.ajax({
-//         url: '/api/get-song',
-//         type: 'GET',
-//         data: {title: search},
-//         success: async function (songData) {
-//             if (songData === null || songData === undefined || songData.statusCode === 404) {
-//                 iziToast.error({
-//                     iconUrl: '/images/error.png',
-//                     title: 'Errore',
-//                     message: 'Canzone non trovata',
-//                     position: 'topRight'
-//                 });
-//                 return;
-//             }
-//             if (songList.includes(songData.id)) {
-//                 iziToast.error({
-//                     iconUrl: '/images/error.png',
-//                     title: 'Errore',
-//                     message: 'Canzone già aggiunta',
-//                     position: 'topRight'
-//                 });
-//                 return;
-//             }
-//
-//
-//             if (data['numero'] === 1)
-//                 element.innerHTML = '';
-//
-//             data['songId'] = songData.id;
-//             data['titolo'] = songData.name;
-//             data['autore'] = songData.artists[0].name;
-//             data['logo'] = songData.album.images[0].url;
-//             data['durata'] = new Date(songData.duration_ms);
-//             data['releaseDate'] = songData.album.release_date;
-//             data['genre'] = await getGenre(data['autore']);
-//             data['songPreview'] = songData.preview_url;
-//             const releaseDate = data['releaseDate'];
-//             const year = releaseDate.substring(0, 4);
-//
-//             //data['genre'] = "Non fornito";
-//             data['durata'] = data['durata'].getMinutes() + ':' + (data['durata'].getSeconds() < 10 ? '0' + data['durata'].getSeconds() : data['durata'].getSeconds());
-//             var song = `<div class="list-group-item">
-//     <div class="row g-2 align-items-center">
-//         <input type="hidden" name="songId[]" value="${data['songId']}">
-//         <input type="hidden" name="songName[]" value="${data['titolo']}">
-//         <input type="hidden" name="songImage[]" value="${data['logo']}">
-//         <input type="hidden" name="songAuthor[]" value="${data['autore']}">
-//         <input type="hidden" name="songDuration[]" value="${data['durata']}">
-//         <input type="hidden" name="songReleaseDate[]" value="${data['releaseDate']}">
-//         <input type="hidden" name="songGenre[]" value="${data['genre']}">
-//         <input type="hidden" name="songPreview[]" value="${data['songPreview']}">
-//         <div class="col-auto fs-3">${data['numero']}</div>
-//         <div class="col-auto"><img src="${data['logo']}" class="rounded"
-//                                    alt="${data['titolo']}" width="40" height="40"></div>
-//         <div class="col">${data['titolo']}
-//         <div class="text-muted">${data['autore']} - ${year} - ${data['genre'] ?? 'Non definito'}</div>
-//         </div>
-//         <div class="col-auto text-muted">${data['durata']}</div>
-//         <svg xmlns="http://www.w3.org/2000/svg"
-//              class="icon icon-tabler icon-tabler-trash g-1" width="40" height="40"
-//              viewBox="0 0 24 24" stroke-width="2" stroke="red" fill="none"
-//              stroke-linecap="round" stroke-linejoin="round"
-//              onclick="deleteMe(this)">
-//             <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-//             <path d="M4 7l16 0"></path>
-//             <path d="M10 11l0 6"></path>
-//             <path d="M14 11l0 6"></path>
-//             <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12"></path>
-//             <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3"></path>
-//         </svg>
-//     </div>
-// </div>`
-//
-//             songList.push(data['songId']);
-//             element.insertAdjacentHTML('beforeend', song);
-//             data['numero'] = data['numero'] + 1;
-//             $('#songSearchInput').val('');
-//         }
-//     });
-// });
 
 
 async function getGenre(autore) {
